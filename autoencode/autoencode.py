2ccfff20cb66d8d91b7410d3eb08d9093e3a88e4
McSwindler <wilingua@gmail.com>
2015-05-03 19:02:38 -0500

    added support for new file types

--- autoencode/autoencode.py
+++ autoencode/autoencode.py
@@ -14,9 +14,10 @@ import enzyme
 from datetime import datetime
 import sqlite3 as lite
 
-FILE_TYPES = ['.mkv']
+FILE_TYPES = ['.mkv', '.iso', '.vob', '.ts', 'VIDEO_TS']
 
 class AutoEncode:
+    file = None
     def __init__(self, args):
         for directory in args.indir:
             if not os.path.exists(directory):
@@ -30,26 +31,33 @@ class AutoEncode:
         self._args = args
         self.file = self.getNewFile()
         if self.file:
-            print "New File: %s" %self.file
-            if self._args.tempdir:
-                print "Copying File..."
-                shutil.copy(self.file, self._args.tempdir)
-                self.file = self._args.tempdir + os.sep + os.path.basename(self.file)
+            print("New File: %s" %self.file['file'])
+            #if self._args.tempdir:
+            #    print("Copying File...")
+            #    shutil.copy(self.file, self._args.tempdir)
+            #    self.file = self._args.tempdir + os.sep + os.path.basename(self.file)
                 
-            print "Starting HandBrake"
-            outfile = self.handbrake(self.file)
+            print("Starting HandBrake")
+            outfile = self.handbrake(self.file['file'])
             if outfile:
-                self._complete(self.file, outfile)
+                self._complete(outfile)
             else:
-                self._fail()
+                print("HandBrake Failed, please check file: %s" % self.file['file'])
                 
             self.file = None
                 
         else:
-            print "No Files to Process"
+            print("No Files to Process")
             
     def __del__(self):
-        self._fail()
+        if not self.file:
+            return
+        
+        con = self._connect() 
+        with con:
+            cur = con.cursor()
+            cur.execute('DELETE FROM videos WHERE name = ?', [self.file.name])
+            con.commit()
         
     def getNewFile(self, index=None):
         if index is None:
@@ -66,33 +74,25 @@ class AutoEncode:
                 if len(files) > 0:
                     f = files[0]
                     cur = con.cursor()
-                    cur.execute('INSERT INTO videos (name, date_started) VALUES (?, ?)', (os.path.basename(f), str(datetime.now())))
+                    cur.execute('INSERT INTO videos (name, date_started) VALUES (?, ?)', (f['name'], str(datetime.now())))
                     con.commit()
                     return f
         return None
     
-    def _complete(self, infile, outfile):
+    def _complete(self, outfile):
         if self._args.tempdir:
-            print "Copying File..."
+            print("Copying File...")
             shutil.copy(outfile, self._args.outdir)
         con = self._connect() 
         with con:
             cur = con.cursor()
-            cur.execute('UPDATE videos SET date_completed = ? WHERE name = ?', (str(datetime.now()), os.path.basename(infile)))
-            con.commit()
-            
-    def _fail(self):
-        if not self.file:
-            return
-        
-        con = self._connect() 
-        with con:
-            cur = con.cursor()
-            cur.execute('DELETE FROM videos WHERE name = ?', (os.path.basename(self.file)))
+            cur.execute('UPDATE videos SET date_completed = ? WHERE name = ?', (str(datetime.now()), self.file['name']))
             con.commit()
                 
     def handbrake(self, infile):
-        cmd = list(self._args.options)
+        cmd = list()
+        if self._args.options:
+            cmd = list(self._args.options)
         outfile = self._outputFile(infile)
         cmd.insert(0, outfile)
         cmd.insert(0, '-o')
@@ -116,7 +116,7 @@ class AutoEncode:
                 if mkv.info.title:
                     filename = mkv.info.title
                     
-        filename =  filename + '.mkv'
+        filename =  filename + '.' + self._args.filetype
         if self._args.tempdir:
             return self._args.tempdir + os.sep + 'encoding' + os.sep + filename
         else:
@@ -128,17 +128,31 @@ class AutoEncode:
         for subdir, dirs, files in os.walk(indir):
             if subdir == self._args.outdir or subdir == self._args.tempdir:
                 continue
+                   
+            isTSGood = True 
             for f in files:
                 f = os.path.join(subdir, f)
-                name = os.path.basename(f)
+                name = f.replace(indir, '')
                 ext = os.path.splitext(name)[1]
-                if ext not in FILE_TYPES or name in dbFiles:
-                    continue
                 try:
                     open(f)
-                    flist.append(f)
-                except IOError, e:
-                    print e
+                except IOError:
+                    print(sys.exc_info()[0])
+                    isTSGood = False
+                    continue
+                
+                if ext not in FILE_TYPES or name in dbFiles:
+                    continue
+                fObj = {"file":f, "indir": indir, "name": name, "ext": ext}
+                flist.append(fObj)
+                
+                
+            if subdir.endswith('VIDEO_TS') and isTSGood:
+                name = subdir.replace(indir, '')
+                if name not in dbFiles:
+                    fObj = {"file":subdir, "indir": indir, "name": name, "ext": "VIDEO_TS"}
+                    flist.append(fObj)
+                
         return flist
     
     def _getFilesFromDb(self, con):
@@ -155,7 +169,7 @@ class AutoEncode:
             cur = con.cursor()
             cur.execute("CREATE TABLE IF NOT EXISTS videos(name TEXT, date_started TEXT, date_completed TEXT)")
         except lite.Error:
-            print traceback.format_exc()
+            print(traceback.format_exc())
             if con:
                 con.close()
         return con       
